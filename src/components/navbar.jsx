6eab422adb1695732eeff2b4a22947643d0658d7
Brett Fischler <brett.fischler@gmail.com>
2018-12-23 14:22:23 -0500

    Add object destructuring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
-// Stateless functional class
-const NavBar = props => {
+// Stateless functional component
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
